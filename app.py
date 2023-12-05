dd8b2d428b3b98af5ade9f84e172ca3e8f78340c
NADIR Hicham <hnadir.dev@gmail.com>
2023-12-05 16:35:04 +0100

    Elasticsearch connection script updated

--- app.py
+++ app.py
@@ -7,35 +7,8 @@ from asgiref.wsgi import WsgiToAsgi
 
 app = Flask(__name__)
 
-# Log transport details (optional):
 
 logging.basicConfig(level=logging.INFO)
-
-# Parse the auth and host from env:
-
-#bonsai = os.environ["https://i1gd3mbo4r:sf8q52fulp@hn-search-9858436033.us-east-1.bonsaisearch.net:443"]
-# bonsai = str("https://i1gd3mbo4r:sf8q52fulp@hn-search-9858436033.us-east-1.bonsaisearch.net:443")
-# auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
-# host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
-
-# # Optional port
-
-# match = re.search('(:\d+)', host)
-# if match:
-#     p = match.group(0)
-#     host = host.replace(p, '')
-#     port = int(p.split(':')[1])
-# else:
-#     port=443
-
-# Connect to cluster over SSL using auth for best security:
-
-# es_header = [{
-# 'host': host,
-# 'port': port,
-# 'use_ssl': True,
-# 'http_auth': (auth[0],auth[1])
-# }]
 
 es_header = [{
 'host': 'https://hn-search-9858436033.us-east-1.bonsaisearch.net',
@@ -43,9 +16,6 @@ es_header = [{
 'use_ssl': True,
 'http_auth': ("i1gd3mbo4r","sf8q52fulp")
 }]
-# Create the client instance
-# Instantiate the new Elasticsearch connection:
-#es = Elasticsearch(es_header)
 
 client = Elasticsearch(
         ['https://hn-search-9858436033.us-east-1.bonsaisearch.net:443'],
@@ -54,7 +24,7 @@ client = Elasticsearch(
 @app.route("/")
 def index():
     # Successful response!
-    #client.info()
+
     return client.info()
 
 
